d8ad6679a9b64edaf5a4f168a72d1710a7768e6a
rmatiz84 <r.matiz@uniandes.edu.co>
2022-04-10 18:33:25 -0500

    se actualiza la url de conexion

--- config/default.py
+++ config/default.py
@@ -1,5 +1,5 @@
 class Config(object):
     TESTING  =  False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///development.db"
-    #SQLALCHEMY_DATABASE_URI = "postgresql://<user>:<password>@<server>:5432/<database>"
+    #SQLALCHEMY_DATABASE_URI = f"sqlite:///development.db"
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@database-1.cpcc3xqideqg.us-east-1.rds.amazonaws.com:5432/postgres'
